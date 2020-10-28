6dfde772a615089ca8ad257e2a4bfd3b8b30e35c
ruben <rubenmartinezlozano@gmail.com>
2020-10-28 18:31:58 +0100

    vDefaultSite.

--- provision.sh
+++ provision.sh
@@ -1,5 +1,6 @@
 apt-get update
 apt-get install nginx
-rm /usr/share/nginx/www/*.*
-ln -s /vagrant/www /usr/share/nginx/www
+rm -rf /etc/nginx/sites-enabled
+cp -r /vagrant/sites-enabled /etc/nginx/
+rm -rf /usr/share/nginx/www
 service nginx start
